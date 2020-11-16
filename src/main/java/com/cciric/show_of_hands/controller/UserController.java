3d9b0fa70707f7b7bbf44fba469e40c8d22a0ada
Chris <cciric44@gmail.com>
2020-11-15 19:32:33 -0500

    added controller methods for getting specific user and getting all users

--- src/main/java/com/cciric/show_of_hands/controller/UserController.java
+++ src/main/java/com/cciric/show_of_hands/controller/UserController.java
@@ -4,10 +4,9 @@ import com.cciric.show_of_hands.data.entity.UserEntity;
 import com.cciric.show_of_hands.models.User;
 import com.cciric.show_of_hands.service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,5 +18,15 @@ public class UserController {
     @PostMapping
     public User addUser(@RequestBody UserEntity entity){
         return userService.createUser(entity);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(value = "/{id}")
+    public User getUser(@PathVariable("id") int id){
+        return userService.getUser(id);
     }
 }
--- src/main/java/com/cciric/show_of_hands/service/service/UserService.java
+++ src/main/java/com/cciric/show_of_hands/service/service/UserService.java
@@ -7,6 +7,9 @@ import com.cciric.show_of_hands.service.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service("UserService")
 public class UserService {
     @Autowired
@@ -23,4 +26,19 @@ public class UserService {
 
     }
 
+    public List<User> getAllUsers() {
+        List<UserEntity> entityList = userRepo.findAll();
+        List<User> modelList = new ArrayList<>();
+
+        entityList.stream().forEach(entity -> {
+            modelList.add(userMapper.entityToModel(entity));
+        });
+        return modelList;
+    }
+
+    public User getUser(int id) {
+        UserEntity entity = userRepo.getOne(id);
+
+        return userMapper.entityToModel(entity);
+    }
 }
