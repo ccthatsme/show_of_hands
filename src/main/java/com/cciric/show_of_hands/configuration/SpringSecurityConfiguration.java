fbd07abca5c6427ba11c7fe8699098070704cf52
Chris <cciric44@gmail.com>
2021-04-13 23:27:37 -0400

    added a cors filter trying to figure out what to do with it

--- src/main/java/com/cciric/show_of_hands/configuration/SpringSecurityConfiguration.java
+++ src/main/java/com/cciric/show_of_hands/configuration/SpringSecurityConfiguration.java
@@ -13,8 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -44,10 +50,10 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable()
+       http.cors().and()
                .authorizeRequests()
                .antMatchers("/users", "/users/**").authenticated()
-               .antMatchers("/authenticate")
+               .antMatchers("/authenticate", "/**")
                .permitAll().anyRequest().authenticated()
                .and().formLogin().loginPage("http://localhost:8000/login.html").permitAll()
                //if any exception occus call this
@@ -60,6 +66,25 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(customJwtAuthenticationFilter,
                 UsernamePasswordAuthenticationFilter.class);
 
-        http.cors().disable();
+
+    }
+
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000/"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
